f8d77de9da72af40bec6ac85dfcc1188162103e0
Jaci R <jaci.brunning@gmail.com>
2017-10-15 21:41:21 +0800

    Add some comments

--- src/main/groovy/jaci/gradle/deploy/DeployExtension.groovy
+++ src/main/groovy/jaci/gradle/deploy/DeployExtension.groovy
@@ -20,18 +20,23 @@ class DeployExtension {
         this.deployers = deployers
 
         this.targets.all { RemoteTarget target ->
+            // Discover the Remote Target on the network
             def discover = project.tasks.create("discover${target.name.capitalize()}".toString(), TargetDiscoveryTask) { TargetDiscoveryTask task ->
                 task.group = "EmbeddedTools"
                 task.description = "Determine the address(es) of target ${target.name.capitalize()}"
                 task.target = target
             }
 
+            // Run the actual deploy. This is what deploy logic, in order to keep SSH sessions at a minimum
+            // Will only run for 'active' deployers
             def runDeploy = project.tasks.create("runDeploy${target.name.capitalize()}", TargetDeployTask) { TargetDeployTask task ->
                 task.deployers = this.deployers
                 task.target = target
                 task.dependsOn discover
             }
 
+            // Configure deployers for deploy. Sets all deployers that respond to this target as active.
+            // Runs runDeploy after configuration.
             project.tasks.create("deploy${target.name.capitalize()}", TargetDeployConfigureTask) { TargetDeployConfigureTask task ->
                 task.group = "EmbeddedTools"
                 task.description = "Deploy all deployers for ${target.name.capitalize()} target"
@@ -44,6 +49,8 @@ class DeployExtension {
         this.deployers.all { Deployer deployer ->
             deployer.project = project
 
+            // Configure this deployer as active for all targets it can respond to.
+            // Runs runDeploy after configuration for each target
             project.tasks.create("deploy${deployer.name.capitalize()}", DeployerDeployConfigureTask) { DeployerDeployConfigureTask task ->
                 task.group = "EmbeddedTools"
                 task.description = "Deploy the ${deployer.name.capitalize()} deployer on all targets"
@@ -58,6 +65,8 @@ class DeployExtension {
             }
         }
 
+        // Configures all deployers as active.
+        // Runs runDeploy after configuration for all targets
         project.tasks.create("deploy", MasterDeployTask) { MasterDeployTask task ->
             task.group = "EmbeddedTools"
             task.description = "Deploy all deployers for all targets"
